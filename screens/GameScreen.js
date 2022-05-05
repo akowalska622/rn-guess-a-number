a5dca5015826514255a7093e4b32d10a4d2baefd
Ania Kowalska <a.kowalska@qarbon.it>
2022-05-05 12:29:08 +0200

    Starting Work on the Game Screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,20 @@
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export const GameScreen = () => {
-	return <Text>Game screen</Text>;
+	return (
+		<View style={styles.screen}>
+			<Text>Opponent's Guess</Text>
+			<View>
+				<Text>Higher or lower?</Text>
+			</View>
+			<View></View>
+		</View>
+	);
 };
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		padding: 12,
+	},
+});
