848b25d7eb8b1401bfadebe29df4ad36e23f5a24
Ania Kowalska <a.kowalska@qarbon.it>
2022-05-05 16:06:52 +0200

    setting dynamic widths

--- components/Title.js
+++ components/Title.js
@@ -7,11 +7,13 @@ export const Title = ({ children }) => (
 const styles = StyleSheet.create({
 	title: {
 		fontSize: 18,
-		fontWeight: 'bold',
+		// fontWeight: 'bold',
 		color: 'white',
 		textAlign: 'center',
 		borderWidth: 2,
 		borderColor: 'white',
 		padding: 12,
+		maxWidth: '80%',
+		width: 300,
 	},
 });
