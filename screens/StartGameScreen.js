eed6bbfa01d903c3e860d8de04c98e1174077d0b
Ania Kowalska <a.kowalska@qarbon.it>
2022-05-05 09:40:03 +0200

    styling a bit

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import { PrimaryButton } from '../components/PrimaryButton';
 
 export const StartGameScreen = () => {
 	return (
-		<View  style={styles.inputContainer}>
+		<View style={styles.inputContainer}>
 			<TextInput />
 			<PrimaryButton>Reset</PrimaryButton>
 			<PrimaryButton>Confirm</PrimaryButton>
@@ -13,8 +13,18 @@ export const StartGameScreen = () => {
 
 const styles = StyleSheet.create({
 	inputContainer: {
-		flex: 1,
 		marginTop: 100,
+		marginHorizontal: 24,
 		padding: 16,
+		backgroundColor: '#72063c',
+		borderRadius: 8,
+		elevation: 4,
+		shadowColor: 'black',
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 6,
+		shadowOpacity: 0.25,
 	},
 });
+
+// elevation <-- ANDROID
+// shadowX <-- IOS
